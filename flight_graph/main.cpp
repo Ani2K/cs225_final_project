ce355075a87213eaf570c88abdc787c89bff2cba
Anirudh Avadhani <asa8@linux-a2.ews.illinois.edu>
2020-12-10 18:37:49 -0600

    fixed output file structure

--- flight_graph/main.cpp
+++ flight_graph/main.cpp
@@ -11,10 +11,8 @@ int main()
     Graph graph = Graph("data/airports.dat", "data/routes.dat");
     Graph::Vertex start = graph.getVertex("Kasese Airport");
     vector<Graph::Vertex> traversal = graph.bfs(start);
-    //vector<Graph::Vertex> traversal2 = graph.bfs2(start);
 
     graph.write_bfs(traversal, "output/bfs_output/bfs_run1.dat");
-    //graph.write_bfs(traversal2, "output/bfs_output/bfs_run2.dat");
 
     return 0;
 }
